4cf3d9c6e1a5d23f56eb7955d924594fc08f3efd
adrianosmond <adrian.osmond@ev-box.com>
2021-02-20 09:34:02 +0100

    Super basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 const App = () => {
   const columns = [
     {
@@ -22,22 +24,72 @@ const App = () => {
   ];
 
   return (
-    <div>
-      <h1>Trello clone</h1>
-      <ul>
+    <Wrapper>
+      <Title>Trello clone</Title>
+      <Grid>
         {columns.map((col) => (
-          <li key={col.id}>
+          <Column key={col.id}>
             <h2>{col.name}</h2>
-            <ul>
+            <ItemList>
               {col.items.map((item) => (
-                <li key={item.id}>{item.name}</li>
+                <Card key={item.id}>{item.name}</Card>
               ))}
-            </ul>
-          </li>
+            </ItemList>
+          </Column>
         ))}
-      </ul>
-    </div>
+      </Grid>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & > * + * {
+    margin-top: 2rem;
+  }
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-weight: 600;
+  font-size: 2rem;
+`;
+
+const Grid = styled.ul`
+  display: flex;
+  padding: 0.25rem;
+  overflow-x: auto;
+  max-width: 100%;
+  & > * + * {
+    margin-left: 1rem;
+  }
+`;
+
+const Card = styled.div`
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  box-shadow: 0 0 0.25rem rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+
+  & > * + * {
+    margin-top: 0.5rem;
+  }
+`;
+
+const Column = styled(Card)`
+  width: 18rem;
+  flex-shrink: 0;
+`;
+
+const ItemList = styled.ul`
+  background-color: #ccc;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+  & > * + * {
+    margin-top: 0.5rem;
+  }
+`;
 
 export default App;
