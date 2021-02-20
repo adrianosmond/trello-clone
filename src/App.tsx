b3ed549f55dd15ef5f9f30ea5ea41e0876175f11
adrianosmond <adrian.osmond@ev-box.com>
2021-02-20 09:28:16 +0100

    Hard coded state

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,43 @@
-const App = () => <div>Trello clone</div>;
+const App = () => {
+  const columns = [
+    {
+      id: 'col-1',
+      name: 'To do',
+      items: [
+        { id: 'item-1', name: 'First' },
+        { id: 'item-2', name: 'Second' },
+        { id: 'item-3', name: 'Third' },
+      ],
+    },
+    {
+      id: 'col-2',
+      name: 'In progress',
+      items: [],
+    },
+    {
+      id: 'col-3',
+      name: 'Done',
+      items: [],
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Trello clone</h1>
+      <ul>
+        {columns.map((col) => (
+          <li key={col.id}>
+            <h2>{col.name}</h2>
+            <ul>
+              {col.items.map((item) => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
